20559667091c38ca267f5ab9fc5def8d8f1bf570
Oleg <wowgameoleg@mail.ru>
2021-05-24 17:31:43 +0300

    implemented some more Kotlin sugar

--- app/src/main/java/com/example/weather/view/view/model/RepositoryImpl.kt
+++ app/src/main/java/com/example/weather/view/view/model/RepositoryImpl.kt
@@ -1,14 +1,8 @@
 package com.example.weather.view.view.model
 
 class RepositoryImpl : Repository {
-    override fun getWeatherFromServer() : Weather {
-        return Weather()
-    }
-
-    override fun getWeatherFromLocalSourceRus() : List<Weather> {
-        return getRussianCities()
-    }
-    override fun getWeatherFromLocalSourceWorld() : List<Weather> {
-        return getWorldCities()
+    override fun getWeatherFromServer() = Weather()
+    override fun getWeatherFromLocalSourceRus() = getRussianCities()
+    override fun getWeatherFromLocalSourceWorld() = getWorldCities()
     }
 }
